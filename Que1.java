51602d6b6d455af8a329cb1cb251665df8161bb6
RAj Patel <rajphx299@gmail.com>
2022-03-17 14:33:47 +0530

    2:33

---
+++ Que1.java
@@ -0,0 +1,31 @@
+import org.w3c.dom.Node;
+
+/**
+ * InsertFirst() to insert an element at the beginning of the list
+ * Insert the data at given position and call the function and debug your program.
+ * insertLast() to insert an element at the end of the list.
+ * isEmpty() and check if the list is empty or not.
+ * Iteratively implement functions for deletions and check your program. Sorted insertion
+ * isLastElement()
+ */
+ 
+ /**
+  * Que1
+  */
+ public class Que1 {
+     public static void main(String[] args) {
+         
+     }
+ }
+
+ /**
+  * ll
+  */
+  class ll {
+ 
+     static class node{
+         int data ;
+         Node next ;
+         void Node(int d) {data=d; next = null ;}
+     }
+ }
