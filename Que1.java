c61ab82410210122f3f196937c4ff19a64666686
RAj Patel <rajphx299@gmail.com>
2022-03-23 14:21:46 +0530

    Vr

--- Que1.java
+++ Que1.java
@@ -16,7 +16,7 @@
  
          if (list.head == null) {
              list.head = new_node;
-         }
+         }  
          else {
              Node last = list.head;
              while (last.next != null) {
@@ -27,7 +27,27 @@
  
          return list;
      }
+     
+     public void insertAfter(Node prev_node, int new_data)
+    {
+    /* 1. Check if the given Node is null */
+    if (prev_node == null) {
+        System.out.println(
+            "The given previous node cannot be null");
+        return;
+    }
  
+    /* 2. Allocate the Node &
+    3. Put in the data*/
+    Node new_node = new Node(new_data);
+ 
+    /* 4. Make next of new Node as next of prev_node */
+    new_node.next = prev_node.next;
+ 
+    /* 5. make next of prev_node as new_node */
+    prev_node.next = new_node;
+    }
+
      public static int isEmpty(LinkedList list){
         if (list.head==null) {
             return 1 ;
@@ -43,7 +63,7 @@
      
          while (currNode != null) {
              System.out.print(currNode.data + " ");
-     
+
              currNode = currNode.next;
          }
      }
@@ -66,5 +86,5 @@
          printList(list);   
      }  
  }
- 
+
  
